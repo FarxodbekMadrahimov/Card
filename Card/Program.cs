6e9e1a95c833a45a0909c01daf6d59a45bd0a42c
farhodbek235 <110379042+farhodbek235@users.noreply.github.com>
2023-06-25 05:40:58 -0700

    Add project files.

---
+++ Card/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Card
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Uzcard uzcard = new Uzcard(7777, "Farxodbek", 100000);
+            var money = uzcard.AddWithBonus(15000);
+            Console.WriteLine(uzcard.Pay(100000));
+
+            Humo humo = new Humo(7777, "Farxod", 50000);
+            Console.WriteLine(humo.PayWireless(51000));
+        }
+    }
+}
